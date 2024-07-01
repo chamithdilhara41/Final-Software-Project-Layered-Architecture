b082b06ebb10f67637b758394d7e8e13a733c9c3
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-07-01 11:02:01 +0530

    apply layered for my project

--- src/main/java/lk/ijse/bo/custom/impl/SupplierStockDetailBOImpl.java
+++ src/main/java/lk/ijse/bo/custom/impl/SupplierStockDetailBOImpl.java
@@ -3,7 +3,6 @@ package lk.ijse.bo.custom.impl;
 import lk.ijse.bo.custom.SupplierStockDetailBO;
 import lk.ijse.dao.DAOFactory;
 import lk.ijse.dao.custom.SupplierStockDetailDAO;
-import lk.ijse.dao.custom.impl.SupplierStockDetailDAOImpl;
 import lk.ijse.entity.SupplierStockDetail;
 import lk.ijse.tdm.SupplierStockDetailTm;
 
